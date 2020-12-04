2b46a9106485a8d42f3b8e63149e9ec05ad331f6
Mark Sanders <mark.a.sanders@capgemini.com>
2020-12-04 16:49:37 +0000

    Revert test failure

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -45,7 +45,7 @@ class ValidatorTests {
 
 		LocaleContextHolder.setLocale(Locale.ENGLISH);
 		Person person = new Person();
-		person.setFirstName("first name not empty");
+		person.setFirstName("");
 		person.setLastName("smith");
 
 		Validator validator = createValidator();
